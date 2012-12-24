975d0edbd1a97ad030b6a9ac171f6e8109f36b1c
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2012-12-24 16:43:14 +0100

    Implementing PgConn.Prepare()

--- src/pger/driver/driver.go
+++ src/pger/driver/driver.go
@@ -1,6 +1,5 @@
 package driver
 
-import "C"
 import "database/sql"
 import "database/sql/driver"
 import "errors"
@@ -9,6 +8,8 @@ func init() {
 	d := new(PgDriver)
 	sql.Register("postgresql", d)
 }
+
+// Implementation of the Conn interface
 
 func (c *PgConn) Begin() (driver.Tx, error) {
 	return nil, errors.New("pger: Begin not implemented")
@@ -19,8 +20,57 @@ func (c *PgConn) Close() error {
 }
 
 func (c *PgConn) Prepare(query string) (driver.Stmt, error) {
-	return nil, errors.New("pger: Prepare not implemented")
+	stmt, err := prepare(c, query)
+	return *stmt, err
 }
+
+// Implementation of the Stmt interface
+
+func (s PgStmt) Close() error {
+	pqclear(s.result)
+	return nil
+}
+
+func (s PgStmt) NumInput() int {
+	return -1
+}
+
+func (s PgStmt) Exec(args []driver.Value) (driver.Result, error) {
+	return PgResult{}, errors.New("pger: Exec not implemented")
+}
+
+func (s PgStmt) Query(args []driver.Value) (driver.Rows, error) {
+	return PgRows{}, errors.New("pger: Query not implemented")
+}
+
+// Implementation of the Result interface
+
+func (r PgResult) LastInsertId() (int64, error) {
+	return 0, nil
+}
+
+func (r PgResult) RowsAffected() (int64, error) {
+	return 1, nil
+}
+
+// Implementation of the Rows interface
+
+func (r PgRows) Columns() []string {
+	// TODO
+	return []string{}
+}
+
+func (r PgRows) Close() error {
+	// TODO
+	return nil
+}
+
+func (r PgRows) Next(dest []driver.Value) error {
+	// TODO
+	return nil
+}
+
+// Implementation of the Driver interface
 
 type PgDriver struct {
 }
--- src/pger/driver/pq.go
+++ src/pger/driver/pq.go
@@ -5,10 +5,13 @@ package driver
 #cgo CFLAGS: -I/usr/include/postgresql
 #cgo LDFLAGS: -lpq
 #include <libpq-fe.h>
+#include <stdlib.h>
 */
 import "C"
 
+import "errors"
 import "fmt"
+import "unsafe"
 
 type PgConn struct {
 	conninfo string
@@ -17,6 +20,7 @@ type PgConn struct {
 
 func connect(conninfo string) (*PgConn, error) {
 	cs := C.CString(conninfo)
+	defer C.free(unsafe.Pointer(cs))
 	conn := C.PQconnectdb(cs)
 	if C.PQstatus(conn) != C.CONNECTION_OK {
 		cerr := C.PQerrorMessage(conn)
@@ -25,5 +29,51 @@ func connect(conninfo string) (*PgConn, error) {
 		return nil, err
 	}
 
+	// one-line error message
+	C.PQsetErrorVerbosity(conn, C.PQERRORS_TERSE)
+
 	return &PgConn{conn: conn, conninfo: conninfo}, nil
+}
+
+type PgResult struct {
+}
+
+type PgStmt struct {
+	name string
+	result *C.PGresult
+}
+
+type PgRows struct {
+}
+
+func prepare(conn *PgConn, query string) (*PgStmt, error) {
+	name := C.CString("")
+	defer C.free(unsafe.Pointer(name))
+	cquery := C.CString(query)
+	defer C.free(unsafe.Pointer(cquery))
+	res := C.PQprepare(conn.conn, name, cquery, 0, nil)
+
+	if err := errorFromPGresult(res); err != nil {
+		pqclear(res)
+		return &PgStmt{}, err
+	}
+
+	stmt := PgStmt{result: res, name: ""}
+	return &stmt, nil
+}
+
+func errorFromPGresult(res *C.PGresult) error {
+	switch C.PQresultStatus(res) {
+	case C.PGRES_EMPTY_QUERY:
+		return errors.New("empty query")
+	case C.PGRES_BAD_RESPONSE:
+		return errors.New("bad response")
+	case C.PGRES_FATAL_ERROR:
+		return errors.New(C.GoString(C.PQresultErrorMessage(res)))
+	}
+	return nil
+}
+
+func pqclear(res *C.PGresult) {
+	C.PQclear(res)
 }
